8cddfd527b478c95f7661a28025287c1b7cff72f
James Howell <james.howell@aia.co.uk>
2017-06-11 20:17:18 +0100

    -Retrieving data from the FourSquare API

--- src/js/modules/foursquare.js
+++ src/js/modules/foursquare.js
@@ -10,7 +10,36 @@ export default (config) => class fourSquare {
 	getVenuesNear (location) {
 		const apiURL = `https://api.foursquare.com/v2/venues/explore?near=${location}&client_id=${this.clientid}&client_secret=${this.secret}&v=${this.version}&m=${this.mode}`;
 
-		console.log(apiURL);
+
+		return fetch(apiURL)
+			.then((response) => response.json())
+				.then(function(data) {
+
+					console.log(data);
+
+					const venues = data.response.groups[0].items;
+					let venueAddress = "";
+
+					const venuesArray = venues.map((venueObj) =>{
+
+						if (venueObj.venue.location.address.length > 0) {
+							venueAddress = venueObj.venue.location.address;
+						}
+
+					return {
+						name: venueObj.venue.name,
+						address: venueObj.venue.location.address
+					}
+				});
+
+				return venuesArray;
+
+
+			})
+		.catch(function(error) {
+			return "error";
+		});
+
 
 	}
 }
